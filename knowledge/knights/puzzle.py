e13044f93126d129adc7f8e3d659c57af53bf009
riya mustare <115172345+riyamustare@users.noreply.github.com>
2025-01-07 22:34:07 +0530

    Add files via upload

---
+++ knowledge/knights/puzzle.py
@@ -0,0 +1,85 @@
+from logic import *
+
+AKnight = Symbol("A is a Knight")
+AKnave = Symbol("A is a Knave")
+
+BKnight = Symbol("B is a Knight")
+BKnave = Symbol("B is a Knave")
+
+CKnight = Symbol("C is a Knight")
+CKnave = Symbol("C is a Knave")
+
+# General constraints: A person cannot be both a Knight and a Knave.
+# And every person must be either a Knight or a Knave.
+base_knowledge = And(
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)),
+
+    Or(BKnight, BKnave),
+    Not(And(BKnight, BKnave)),
+
+    Or(CKnight, CKnave),
+    Not(And(CKnight, CKnave))
+)
+
+# Puzzle 0
+# A says "I am both a knight and a knave."
+knowledge0 = And(
+    base_knowledge,
+    Implication(AKnight, And(AKnight, AKnave))
+)
+
+# Puzzle 1
+# A says "We are both knaves."
+# B says nothing.
+knowledge1 = And(
+    base_knowledge,
+    Implication(AKnight, And(AKnave, BKnave))
+)
+
+# Puzzle 2
+# A says "We are the same kind."
+# B says "We are of different kinds."
+knowledge2 = And(
+    base_knowledge,
+    Implication(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
+    Implication(BKnight, Or(And(AKnight, BKnave), And(AKnave, BKnight)))
+)
+
+# Puzzle 3
+# A says either "I am a knight." or "I am a knave.", but you don't know which.
+# B says "A said 'I am a knave'."
+# B says "C is a knave."
+# C says "A is a knight."
+knowledge3 = And(
+    base_knowledge,
+
+    # A's statement: Either "I am a knight" or "I am a knave"
+    Implication(AKnight, Or(AKnight, AKnave)),
+
+    # B's statements
+    Implication(BKnight, And(Implication(AKnight, AKnave), CKnave)),
+
+    # C's statement
+    Implication(CKnight, AKnight)
+)
+
+def main():
+    symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
+    puzzles = [
+        ("Puzzle 0", knowledge0),
+        ("Puzzle 1", knowledge1),
+        ("Puzzle 2", knowledge2),
+        ("Puzzle 3", knowledge3)
+    ]
+    for puzzle, knowledge in puzzles:
+        print(puzzle)
+        if len(knowledge.conjuncts) == 0:
+            print("    Not yet implemented.")
+        else:
+            for symbol in symbols:
+                if model_check(knowledge, symbol):
+                    print(f"    {symbol}")
+
+if __name__ == "__main__":
+    main()
